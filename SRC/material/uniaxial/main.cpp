40ecdcf33ddd9b9a3f242bf6d17ac7c30a95ca24
Michael H. Scott <michael.scott@oregonstate.edu>
2025-05-14 17:31:50 -0700

    Updating main test function

--- SRC/material/uniaxial/main.cpp
+++ SRC/material/uniaxial/main.cpp
@@ -30,6 +30,9 @@
 
 #include <HardeningMaterial.h>
 #include <HardeningMaterial2.h>
+#include <Concrete02.h>
+#include <Concrete02IS.h>
+#include <Concrete04.h>
 
 #include <Parameter.h>
 #include <Information.h>
@@ -46,33 +49,40 @@ int main()
   const double Fy0 = 60.0;
   const double E0 = 29000.0;
   const double Hk0 = 500.0;
-  const double Hi0 = 100.0;
+  const double Hi0 = 0.0;
 
   double E = E0;
   double Fy = Fy0;
   double Hk = Hk0;
   double Hi = Hi0;
 
-  HardeningMaterial  matl(0, E, Fy, Hi, Hk);
-  HardeningMaterial2 matl2(0, E, Fy, Hi, Hk);
+  UniaxialMaterial *theMaterial = new HardeningMaterial2(0, E, Fy, Hi, Hk);
+  delete theMaterial;
+  theMaterial = new Concrete02(0, 4, 0.002, 2, 0.006);
+  //theMaterial = new Concrete02IS(0, 3600, 4, 0.002, 2, 0.006);
+  //theMaterial = new Concrete04(0, 4, 0.002, 1, 3600);    
 
   double epsy = Fy/E;
-  double epsmax = 3*epsy;
+  double epsmax = -3*epsy;
   const int Nsteps = 400;
   double deps = epsmax/Nsteps;
 
   Parameter p(1);
   
   const char *argv[1];
-  argv[0] = "Fy";
-  int pid = 1;
-
-  matl.activateParameter(pid);
-  matl.commitSensitivity(0,0,1);
+  double h0;
+  int pid2;
+  //argv[0] = "E"; pid2 = 1; h0 = E0;
+  //argv[0] = "Fy"; pid2 = 2; h0 = Fy0;
+  //  argv[0] = "Hk"; pid2 = 4; h0 = Hk0;
+  argv[0] = "fc"; pid2 = 1; h0 = -4;
+  
+  //matl.activateParameter(pid);
+  //matl.commitSensitivity(0,0,1);
   
   double eplot[2*Nsteps];
   double splot[2*Nsteps];
-  double dsplot[2*Nsteps];
+  double global[2*Nsteps];
   
   for (int i = 0; i < 2*Nsteps; i++) {
     // strain
@@ -80,68 +90,93 @@ int main()
     if (i >= Nsteps)
       eps = epsmax - (i-Nsteps)*deps;
 
+    /*
     // DDM computation
     matl.setTrialStrain(eps);    
     matl.commitState();
     dsplot[i] = matl.getStressSensitivity(0,true);
     matl.commitSensitivity(0,0,1);
-
-    matl2.setTrialStrain(eps);
-    double sig = matl2.getStress();
+    */
+    
+    theMaterial->setTrialStrain(eps);
+    double sig = theMaterial->getStress();
     eplot[i] = eps;
     splot[i] = sig;
-    matl2.commitState();
+    theMaterial->commitState();
+  }
+
+  theMaterial->revertToStart();  
+
+  double dh = 0.001*h0;
+  double h = h0 + dh;
+
+  Information info;
+  info.theDouble = h;
+  theMaterial->updateParameter(pid2, info);
+    
+  for (int i = 0; i < 2*Nsteps; i++) {
+    // strain
+    double eps = i*deps;
+    if (i >= Nsteps)
+      eps = epsmax - (i-Nsteps)*deps;
+
+    theMaterial->setTrialStrain(eps);
+    double sig = theMaterial->getStress();
+    global[i] = (sig-splot[i])/dh;
+    theMaterial->commitState();
   }
 
 
+  
+  theMaterial->revertToStart();
 
-  matl2.revertToStart();
-
-  int numHV = matl2.getNumHistoryVariables();
+  int numHV = theMaterial->getNumHistoryVariables();
 
   double *hstvP = new double[numHV];
   double *hstvP2 = new double[numHV];  
-  matl2.getCommittedHistoryVariables(hstvP);
-  matl2.getCommittedHistoryVariables(hstvP2);  
-  
-  double h0 = Fy0;
-  double dh = 0.001*h0;
-  Fy = Fy0 + dh;
+  theMaterial->getCommittedHistoryVariables(hstvP);
+  theMaterial->getCommittedHistoryVariables(hstvP2);  
 
-  Information info;
 
-  double ffd[2*Nsteps];
+
+
+
+  double mixed[2*Nsteps];
   
   for (int i = 0; i < 2*Nsteps; i++) {
     double eps = i*deps;
     if (i >= Nsteps)
       eps = epsmax - (i-Nsteps)*deps;
 
-    matl2.setCommittedHistoryVariables(hstvP);
-    info.theDouble = Fy0;
-    matl2.updateParameter(pid, info);
-    matl2.setTrialStrain(eps);
+    info.theDouble = h0;
+    theMaterial->updateParameter(pid2, info);
+    theMaterial->setCommittedHistoryVariables(hstvP);
+    theMaterial->setTrialHistoryVariables(hstvP);
+    theMaterial->setTrialStrain(eps);
 
-    double sig = matl2.getStress();
-    matl2.getTrialHistoryVariables(hstvP);
+    double sig = theMaterial->getStress();
+    theMaterial->getTrialHistoryVariables(hstvP);
 
-    matl2.setCommittedHistoryVariables(hstvP2);    
-    info.theDouble = Fy;
-    matl2.updateParameter(pid, info);
-    matl2.setTrialStrain(eps);
-    double sig2 = matl2.getStress();    
-    matl2.getTrialHistoryVariables(hstvP2);
+    info.theDouble = h;
+    theMaterial->updateParameter(pid2, info);
+    theMaterial->setCommittedHistoryVariables(hstvP2);
+    theMaterial->setTrialHistoryVariables(hstvP2);        
+    theMaterial->setTrialStrain(eps);
 
-    matl2.commitState();
+    double sig2 = theMaterial->getStress();    
+    theMaterial->getTrialHistoryVariables(hstvP2);
+
+    theMaterial->commitState();
     
-    ffd[i] = (sig2-sig)/dh;    
+    mixed[i] = (sig2-sig)/dh;    
   }
 
-  //  for (int i = 0; i < 2*Nsteps; i++)
-  //opserr << dsplot[i] << ' ' << ffd[i] << endln;
-  
+  for (int i = 0; i < 4*Nsteps/2; i++)
+    opserr << "sig: " << splot[i] << ", global: " << global[i] << ", mixed: " << mixed[i] << endln;
+
   delete [] hstvP;
   delete [] hstvP2;  
+  delete theMaterial;
   
   return 0;
 }
